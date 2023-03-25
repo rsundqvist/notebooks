aae25754e2d125faba35621f1f0171d05ee9d6c2
Richard Sundqvist <richard.sundqvist@live.se>
2023-03-25 11:39:32 +0100

    Update outdated notebooks

--- id-translation/data.py
+++ id-translation/data.py
@@ -4,7 +4,7 @@ import re
 from pathlib import Path
 
 import pandas as pd
-from rics.utility.misc import get_local_or_remote
+from rics.misc import get_local_or_remote
 
 _LOCAL_ROOT = Path(__file__).parent.joinpath("data-cache")
 
